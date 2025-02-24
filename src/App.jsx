15202f581e44cda425327c8eb05f44f65e16916a
MD Jobayer Adnan <you@example.com>
2025-02-24 12:43:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-5xl'>all is well</h1>
+    <button className="btn btn-warning">Warning</button>
+
+    </>
+  )
+}
+
+export default App
