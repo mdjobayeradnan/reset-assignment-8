65bea68e3a26ff85eaf1bf11e79c9780ebc4cc1c
MD Jobayer Adnan <you@example.com>
2025-02-27 21:29:45 +0600

    end the project

--- src/components/BooksSection/BooksSection.jsx
+++ src/components/BooksSection/BooksSection.jsx
@@ -17,7 +17,7 @@ const BooksSection = () => {
      },[])
     return (
         <div className="">
-        <h1 className="text-green-500 text-bold   w-[300px] mx-auto text-center text-5xl py-5">BOOKS:{books.length}</h1>
+        <h1 className="text-green-500 text-bold   w-[300px] mx-auto text-center text-5xl py-5">BOOKS</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-5 w-[1200px] mx-auto">
         {
